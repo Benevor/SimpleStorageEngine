9ae027e7e796c7fd06e85cb54e7f8b4ffd4efd9a
Benevor <2647311844@qq.com>
2022-10-16 15:15:55 +0800

    add format files

---
+++ include/storage_engine.h
@@ -0,0 +1,15 @@
+namespace sse {
+class storage_engine {
+ public:
+  storage_engine(/* args */);
+  ~storage_engine();
+
+ private:
+  //
+};
+
+storage_engine::storage_engine(/* args */) {}
+
+storage_engine::~storage_engine() {}
+
+}  // namespace sse
